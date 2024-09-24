a9a406a33ab064ab6cf84757733a697e59dfa7c4
Javi Blazquez <j.blazquez.2019@alumnos.urjc.es>
2024-09-24 12:48:48 +0200

    indent code

--- mini-curso-c/intro.c
+++ mini-curso-c/intro.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int x = 1; /* global variables*/
+int x = 1;			/* global variables */
 int k;
 
 int
-main(int argc, char* argv[])	/*Definition of main function*/
-{
-	int i, q=1, u=12; /*local variables*/
+main(int argc, char *argv[])
+{				/*Definition of main function */
+	int i, q = 1, u = 12;	/*local variables */
 	char c;
 	char p = 'o';
 
 	c = 'z';
-	i =13;
+	i = 13;
 
-	printf("Hello World!\n"); /* print the Hello World!*/
+	printf("Hello World!\n");	/* print the Hello World! */
 	exit(EXIT_SUCCESS);
 }
