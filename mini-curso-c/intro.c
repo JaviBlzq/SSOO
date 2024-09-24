c3a44e8d6b141011484707dc7ea1eed4c82916be
Javi Blazquez <j.blazquez.2019@alumnos.urjc.es>
2024-09-24 12:47:34 +0200

    introduction to local & global variables

--- mini-curso-c/intro.c
+++ mini-curso-c/intro.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int x = 1; /* global variables*/
+int k;
+
 int
-main(){
-    printf("Hello World!\n");
-    exit(EXIT_SUCCESS);
+main(int argc, char* argv[])	/*Definition of main function*/
+{
+	int i, q=1, u=12; /*local variables*/
+	char c;
+	char p = 'o';
+
+	c = 'z';
+	i =13;
+
+	printf("Hello World!\n"); /* print the Hello World!*/
+	exit(EXIT_SUCCESS);
 }
