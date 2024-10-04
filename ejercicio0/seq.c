2cfd26d40e3276f69c5890ed99a7c5f113c594cb
Javi Blazquez <j.blazquez.2019@alumnos.urjc.es>
2024-10-04 17:03:02 +0200

    ejercicio completo. Falta añadir la funcionalidad de flag -w

--- ejercicio0/seq.c
+++ ejercicio0/seq.c
@@ -39,57 +39,75 @@ check_pointer(char *ptr, char *value)
 int
 get_base(char *value)
 {
-    int base = 10;
-    int has_0;
-    int has_x;
-    int correct_len;
-    if (value == NULL) {
-        fprintf(stderr, "Valor nulo");
-    }
-    has_0 = value[0] == '0';
-    correct_len = strlen(value) >= 2;
-    has_x = value[1] == 'x' || value[1] == 'X';
-    if (has_0 && (correct_len && has_x)) {
-        base = 16;  
-    }
-    return base;
+	int base = 10;
+	int has_0;
+	int has_x;
+	int correct_len;
+
+	if (value == NULL) {
+		fprintf(stderr, "Valor nulo");
+	}
+	has_0 = value[0] == '0';
+	correct_len = strlen(value) >= 2;
+	if (correct_len) {
+		has_x = value[1] == 'x' || value[1] == 'X';
+		if (has_0 && (correct_len && has_x)) {
+			base = 16;
+		}
+	}
+
+	return base;
 }
 
 void
-print_numbers(int argc, char *argv[])
+parse_args(int argc, char *argv[], int *arr)
 {
 	int first_value = FIRST_VALUE;
 	int step = INITIAL_STEP;
 	int last_value;
-	int i;
-    int base;
+	int base;
 	char *ptr;
 
 	if (argc == 2) {
-        base = get_base(argv[1]);
+		base = get_base(argv[1]);
 		last_value = strtol(argv[1], &ptr, base);
 		check_pointer(ptr, argv[1]);
+
 	} else if (argc == 3) {
-        base = get_base(argv[1]);
+		base = get_base(argv[1]);
 		first_value = strtol(argv[1], &ptr, base);
 		check_pointer(ptr, argv[1]);
 
-        base = get_base(argv[2]);
+		base = get_base(argv[2]);
 		last_value = strtol(argv[2], &ptr, base);
 		check_pointer(ptr, argv[2]);
 	} else if (argc == 4) {
-        base = get_base(argv[1]);
+		base = get_base(argv[1]);
 		first_value = strtol(argv[1], &ptr, base);
 		check_pointer(ptr, argv[1]);
 
-        base = get_base(argv[2]);
+		base = get_base(argv[2]);
 		step = strtol(argv[2], &ptr, base);
 		check_pointer(ptr, argv[2]);
 
-        base = get_base(argv[3]);
+		base = get_base(argv[3]);
 		last_value = strtol(argv[3], &ptr, base);
 		check_pointer(ptr, argv[3]);
 	}
+	arr[0] = first_value;
+	arr[1] = step;
+	arr[2] = last_value;
+
+}
+
+void
+print_numbers(int *values)
+{
+	int first_value = values[0];
+	int step = values[1];
+	int last_value = values[2];
+	int i;
+
 	if (correct_values(first_value, last_value)) {
 		for (i = first_value; i <= last_value; i += step) {
 			printf("%d\n", i);
@@ -100,7 +118,10 @@ print_numbers(int argc, char *argv[])
 int
 main(int argc, char *argv[])
 {
+	int arr[3];
+
 	check_args(argc);
-	print_numbers(argc, argv);
+	parse_args(argc, argv, arr);
+	print_numbers(arr);
 	exit(EXIT_SUCCESS);
 }
