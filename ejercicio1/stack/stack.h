c57d68d46617139452c0910bd16c7b41d98276b7
Javi Blazquez <fiteitor1@gmail.com>
2024-10-18 13:20:06 +0200

    indented stack.h and new tgz to send

--- ejercicio1/stack/stack.h
+++ ejercicio1/stack/stack.h
@@ -1,20 +1,19 @@
 enum {
-    MAX_SIZE = 31 + 1, /*31 del límite más '\0'*/
+	MAX_SIZE = 31 + 1,	/*31 del límite más '\0' */
 };
 
 typedef struct Node Node;
 struct Node {
-    char str[MAX_SIZE];
-    Node *next;
+	char str[MAX_SIZE];
+	Node *next;
 };
 
 typedef struct Stack Stack;
-struct Stack
-{
-    Node *tos; /*Top Of Stack*/
+struct Stack {
+	Node *tos;		/*Top Of Stack */
 };
 
-Stack* newstack(void);
+Stack *newstack(void);
 void push(char *str, Stack *s);
-char* pop(Stack *s);
+char *pop(Stack *s);
 void destroystack(Stack *s);
