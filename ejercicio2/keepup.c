c5fdb0ab5478a41d033dcfc08ecc05bc48bf1841
Javi Blazquez <fiteitor1@gmail.com>
2024-10-25 13:59:14 +0200

    fix: corrected exit value from child proccess

--- ejercicio2/keepup.c
+++ ejercicio2/keepup.c
@@ -4,6 +4,12 @@
 #include <sys/wait.h>
 #include <string.h>
 
+
+enum {
+	VALUE_EXIT_ERROR = 200,
+
+};
+
 void
 check_args(int argc, char *argv[])
 {
@@ -37,7 +43,9 @@ get_correct_args(int argc, char *argv[])
 	argv[0] = executable;
 }
 
-void
+
+
+int
 exec_file(int argc, char *argv[])
 {
 	char *path;
@@ -45,13 +53,9 @@ exec_file(int argc, char *argv[])
 	pid_t pid;
 
 	int tries = 0;
-	int exists = 0;
-
 	path = argv[0];
-
 	get_correct_args(argc, argv);
-
-	while (!exists) {
+	while (1) {
 		pid = fork();
 		switch (pid) {
 		case -1:
@@ -60,21 +64,23 @@ exec_file(int argc, char *argv[])
 		case 0:
 			execv(path, argv);
 			fprintf(stderr, "keepup: cannot execute %s\n", path);
-
+			exit(VALUE_EXIT_ERROR);
 		default:
 			if (waitpid(pid, &status, 0) == -1) {
-				perror("waitpid");
+				perror("wait");
 			}
 
 			if (WIFEXITED(status) && (WEXITSTATUS(status) == 0)) {
-				exists = 1;
+				return 0;
+				
+			} else if (WEXITSTATUS(status) == VALUE_EXIT_ERROR){
+				return 1;
 			} else {
 				tries++;
 				fprintf(stderr,
 					"keepup: the command %s exited ntimes: %d\n",
 					path, tries);
 			}
-
 		}
 	}
 }
@@ -82,10 +88,13 @@ exec_file(int argc, char *argv[])
 int
 main(int argc, char *argv[])
 {
+	
 	check_args(argc, argv);
 	argv++;
 	argc--;
 	check_path(argv);
-	exec_file(argc, argv);
+	if (exec_file(argc, argv) == 1){
+		exit(EXIT_FAILURE);
+	}
 	exit(EXIT_SUCCESS);
 }
