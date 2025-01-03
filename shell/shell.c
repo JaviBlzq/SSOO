53dc760bd6d608aa3725cf76972537e7c97dce5a
Javi Blazquez <fiteitor1@gmail.com>
2025-01-03 13:09:40 +0100

    feat: new features to shell. Builtins implemented and subsitutions pre-globbing

--- shell/shell.c
+++ shell/shell.c
@@ -2,16 +2,27 @@
 #include <stdio.h>
 #include <err.h>
 #include <string.h>
+#include <unistd.h>
 
 enum {
     Stdin = 0,
-    Maxlinelen = 256,
+    Maxlinelen = 255,
+    MaxOutputlinelen = 1024,
+    Maxpath = 255,
+    Success = 1,
+    Failure = 0,
 };
 
 int
 correct_args(int argc)
 {
     return argc == 1;
+}
+
+char *
+get_token(char* str_to_tokenize, char* delim, char **saveptr)
+{    
+    return strtok_r(str_to_tokenize, delim, saveptr);
 }
 
 void
@@ -24,43 +35,173 @@ treat_line(char* line)
     /*update line with value of env value*/
 }
 
-char *
-get_token(char* str_to_tokenize, char* delim)
+void
+change_dir(char *dir){
+    if (chdir(dir) != 0){
+        warn("cd error");
+    }
+}
+
+int
+do_cd(char* line){
+    char* cmd;
+    char* dir;
+    char *saveptr = NULL;
+    char *home;
+    cmd = get_token(line, " ", &saveptr);
+    if (cmd != NULL){
+        if (strcmp(cmd, "cd") == 0){
+            dir = get_token(NULL, " ", &saveptr);
+            if (dir != NULL) {
+                change_dir(dir);
+                return Success;
+            } else {
+                if ((home = getenv("HOME") ) != NULL){
+                    change_dir(home);
+                    return Success;
+                } else {
+                    fprintf(stderr, "home not founded");
+                    return Failure;
+                }
+            }
+        }
+    }
+    return Failure;
+}
+
+int
+create_variables(char *line)
 {
-    char *saveptr;
-    char *token;
-    token = strtok_r(str_to_tokenize, delim, &saveptr);
-    
-    return token;
+    char *saveptr = NULL;
+    char *name;
+    char *value;
+    int overwrite = 1;
+    if (strchr(line, '=')){
+        name = get_token(line, "=", &saveptr);
+        value = get_token(NULL, "=", &saveptr);
+        if (name != NULL && value != NULL){
+            if (setenv(name, value, overwrite) == 0){
+                return Success;
+            } else {
+                warn("setenv");
+            }
+        }
+    }
+    return Failure;
+
 }
 
 int 
-is_builtin(char* line){
-    char* cmd;
-    cmd = get_token(line, " ");
-    if (cmd != NULL){
-        cmd++;
-        fprintf(stderr, "%s\n", cmd);
+do_builtins(char* line){
+    char line_copy[Maxlinelen];
+    strncpy(line_copy, line, sizeof(line_copy));
+    line_copy[sizeof(line_copy) - 1] = '\0';
+    if (do_cd(line_copy) == Success || create_variables(line_copy) == Success){
+        return Success;
     }
-    return 1;
+    return Failure;
+}
+
+void
+printline(void)
+{
+    char cwd[Maxpath];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s", cwd);
+    } else {
+        warn("getcwd() error"); 
+    }
+}
+
+int
+run_executable_cwd()
+{
+    return Failure;
+}
+
+void
+run_command(char* line){
+    fprintf(stderr, "Linea %s\n", line);
+    if (run_executable_cwd(line) == Failure){
+        
+    }
+    
+    
+}
+
+void
+sustitute_varenv(char*line, char* output_line, ssize_t output_size)
+{
+    char line_copy[Maxlinelen];
+    char *token, *saveptr;
+    char *env_value;
+    size_t remaining_size = output_size - 1;
+
+    strncpy(line_copy, line, sizeof(line_copy));
+    line_copy[sizeof(line_copy) - 1] = '\0';
+
+    output_line[0] = '\0';
+
+    token = strtok_r(line_copy, " ", &saveptr);
+    while (token != NULL) {
+        if (token[0] == '$') {
+            env_value = getenv(++token);
+            if (env_value) {
+                if (strlen(env_value) + strlen(output_line) + 1 > remaining_size) {
+                    warn("Output buffer overflow");
+                    return;
+                }
+                strncat(output_line, env_value, remaining_size);
+                remaining_size -= strlen(env_value);
+            } else {
+                if (strlen(token) + strlen(output_line) + 1 > remaining_size) {
+                    warn("Output buffer overflow");
+                    return;
+                }
+                strncat(output_line, token, remaining_size);
+                remaining_size -= strlen(token);
+            }
+        } else {
+            if (strlen(token) + strlen(output_line) + 1 > remaining_size) {
+                warn("Output buffer overflow");
+                return;
+            }
+            strncat(output_line, token, remaining_size);
+            remaining_size -= strlen(token);
+        }
+
+        token = strtok_r(NULL, " ", &saveptr);
+        if (token && strlen(output_line) + 1 < remaining_size) {
+            strncat(output_line, " ", remaining_size);
+            remaining_size -= 1;
+        }
+    }
 }
 
 void
 run_shell(void)
 {
     char line[Maxlinelen];
+    char* full_line = (char*)malloc(MaxOutputlinelen * sizeof(char));
     while (1){
         memset(line, 0, Maxlinelen);
+        memset(full_line, 0, Maxlinelen);
+        printline();
         printf("$ ");
         fgets(line, Maxlinelen, stdin);
         treat_line(line);
-        if (is_builtin(line)){
-            /*do builtin*/
+        if (do_builtins(line) != Success){
+            sustitute_varenv(line, full_line, MaxOutputlinelen);
+            run_command(full_line);
+        }
+        /*if (is_builtin(line)){
+            do builtin
             printf("is builtin");
         } else {
             printf("hello");
-        }
+        }*/
     }
+    free(full_line);
 }
 
 int
